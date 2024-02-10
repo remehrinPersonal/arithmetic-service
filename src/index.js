def84f82115f2e5d13d37beb8abaef71cfee4d19
Ross Mehringer <rossmehringer42@gmail.com>
2024-02-09 20:15:04 -0500

    backend with extras

--- src/index.js
+++ src/index.js
@@ -12,4 +12,16 @@ app.get('/add/:n/:m', (req, res) => {
     res.json(Number(req.params.n) + Number(req.params.m));
 })
 
+app.get('/subtract/:n/:m', (req, res) => {
+    res.json(Number(req.params.n) - Number(req.params.m));
+})
+
+app.get('/multiply/:n/:m', (req, res) => {
+    res.json(Number(req.params.n) * Number(req.params.m));
+})
+
+app.get('/divide/:n/:m', (req, res) => {
+    res.json(Number(req.params.n) / Number(req.params.m));
+})
+
 app.listen(port)
